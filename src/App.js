94533494389eea0fc8babcc2b215a7524b918b6c
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2022-05-04 18:33:28 +0900

    レンダリング最適化

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
 
 export default function App() {
   console.log("App");
 
   const [text, setText] = useState("");
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const onChangeText = (e) => {
     setText(e.target.value);
   };
+  const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+  //再レンダリング最適化②　useCallback()で囲む 第2引数に配列記述できる
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+  //再レンダリング最適化③　useMemo()で囲む 第2引数に配列記述できる
 
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
-      <button>表示</button>
-      <ChildArea />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} close={onClickClose} />
+      <InlineStyle />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,7 +1,28 @@
-export const ChildArea = () => {
-  return (
-    <div>
-      <p>子コンポーネント</p>
-    </div>
-  );
+import { memo } from "react";
+import { InlineStyle } from "./components/InlineStyle";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
 };
+
+export const ChildArea = memo(({ open, close }) => {
+  //再レンダリング最適化①　memo()で囲む
+  console.log("ChildAreaがレンダリングされた");
+  // const data = [...Array(2000).keys()];
+  // data.forEach(() => {
+  //   console.log("...");
+  // });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={close}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
